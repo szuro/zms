3bf299962523576fc747772b3919aedf6f5d6ff3
Robert Szulist <r.szulist@gmail.com>
2022-12-09 23:11:16 +0100

    Add event

--- zbx/exports.go
+++ zbx/exports.go
@@ -29,6 +29,11 @@ type Host struct {
 	Name string `json:"name"`
 }
 
+type Tag struct {
+	Tag   string `json:"tag"`
+	Value string `json:"value"`
+}
+
 type History struct {
 	Host   Host `json:"host"`
 	ItemID int  `json:"itemid"`
@@ -49,6 +54,19 @@ type Trend struct {
 	Type          int
 }
 
+type Event struct {
+	Clock    int      `json:"clock"`
+	NS       int      `json:"ns"`
+	Value    int      `json:"value"`
+	EventID  int      `json:"eventid"`
+	PEventID int      `json:"p_eventid"`
+	Name     string   `json:"name,omitempty"`
+	Severity int      `json:"severity,omitempty"`
+	Hosts    []Host   `json:"hosts,omitempty"`
+	Groups   []string `json:"groups,omitempty"`
+	Tags     []Tag    `json:"tags,omitempty"`
+}
+
 type Export interface {
-	History | Trend
+	History | Trend | Event
 }
