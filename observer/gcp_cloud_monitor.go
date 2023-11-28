50a6b47996eb8c70cc9dcbf8833fae54ca2ee2d7
Robert Szulist <r.szulist@gmail.com>
2023-11-28 17:21:38 +0100

    Push history as a single metric to GCP

--- observer/gcp_cloud_monitor.go
+++ observer/gcp_cloud_monitor.go
@@ -2,21 +2,27 @@ package observer
 
 import (
 	"context"
-	"crypto/md5"
-	"encoding/hex"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 
 	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
 	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
-	"github.com/golang/protobuf/ptypes/timestamp"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/option"
+	"google.golang.org/genproto/googleapis/api/label"
+	"google.golang.org/genproto/googleapis/api/metric"
 	metricpb "google.golang.org/genproto/googleapis/api/metric"
 	"google.golang.org/genproto/googleapis/api/monitoredres"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"golang.org/x/oauth2/google"
+
+	"google.golang.org/api/option"
 	"szuro.net/zms/zbx"
 )
+
+const HISTORY_TYPE string = "custom.googleapis.com/zabbix_export/history"
+const TREND_TYPE string = "custom.googleapis.com/zabbix_export/trend"
 
 type CloudMonitor struct {
 	baseObserver
@@ -44,6 +50,8 @@ func NewCloudMonitor(name, file string) (cm *CloudMonitor) {
 	if err != nil {
 		// TODO: Handle error.
 	}
+
+	createHistoryMetric(cm.projectID)
 
 	return
 }
@@ -105,14 +113,12 @@ func newResource() *monitoredres.MonitoredResource {
 }
 
 func itemToMetric(item zbx.History) (m *metricpb.Metric) {
-	hash := md5.Sum([]byte(item.Name))
 	m = &metricpb.Metric{
-		Type: "custom.googleapis.com/" + "zabbix_" + hex.EncodeToString(hash[:]),
+		Type: HISTORY_TYPE,
 		Labels: map[string]string{
-			"item":    item.Name,
-			"itemid":  strconv.Itoa(item.ItemID),
-			"host":    item.Host.Host,
-			"history": "history",
+			"item":   item.Name,
+			"itemid": strconv.Itoa(item.ItemID),
+			"host":   item.Host.Host,
 		},
 	}
 	return
@@ -143,4 +149,52 @@ func newTimeSeries(resource *monitoredres.MonitoredResource, item zbx.History) (
 		Resource: resource,
 	}
 	return
+}
+
+func mkStandardLabels() []*label.LabelDescriptor {
+	return []*label.LabelDescriptor{
+		{
+			Key:         "item",
+			ValueType:   label.LabelDescriptor_STRING,
+			Description: "Name of a Zabbix item",
+		},
+		{
+			Key:         "itemid",
+			ValueType:   label.LabelDescriptor_INT64,
+			Description: "itemid of a Zabbix item",
+		}, {
+			Key:         "host",
+			ValueType:   label.LabelDescriptor_STRING,
+			Description: "Host that contains this item",
+		},
+	}
+}
+
+func createHistoryMetric(projectID string) (*metricpb.MetricDescriptor, error) {
+	ctx := context.Background()
+	c, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+	md := &metric.MetricDescriptor{
+		Name:        "Zabbix history",
+		Type:        HISTORY_TYPE,
+		Labels:      mkStandardLabels(),
+		MetricKind:  metric.MetricDescriptor_GAUGE,
+		ValueType:   metric.MetricDescriptor_DOUBLE,
+		Unit:        "",
+		Description: "Zabbix item history exported via ZMS",
+		DisplayName: "Zabbix history",
+	}
+	req := &monitoringpb.CreateMetricDescriptorRequest{
+		Name:             projectID,
+		MetricDescriptor: md,
+	}
+	m, err := c.CreateMetricDescriptor(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("could not create custom metric: %w", err)
+	}
+
+	return m, nil
 }
