bbf7ece09261e3543ffff2c833f46aa4a8c58ded
Robert Szulist <robert@szuro.net>
2025-09-25 16:19:46 +0200

    Implement custom buffer

---
+++ pkg/plugin/buffer.go
@@ -0,0 +1,161 @@
+package plugin
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log/slog"
+	"path"
+	"time"
+
+	badger "github.com/dgraph-io/badger/v4"
+	"szuro.net/zms/internal/logger"
+	"szuro.net/zms/pkg/zbx"
+)
+
+type ZMSBuffer interface {
+	BufferHistory(history []zbx.History) (err error)
+	FetchHistory(number int) (history []zbx.History, err error)
+	DeleteHistory(history []zbx.History) (err error)
+	BufferTrends(trends []zbx.Trend) (err error)
+	FetchTrends(number int) (trends []zbx.Trend, err error)
+	DeleteTrends(trends []zbx.Trend) (err error)
+	BufferEvents(events []zbx.Event) (err error)
+	FetchEvents(number int) (events []zbx.Event, err error)
+	DeleteEvents(events []zbx.Event) (err error)
+	InitBuffer(bufferPath string, t int64)
+	Cleanup()
+}
+
+type ZMSDefaultBuffer struct {
+	offlineBufferTTL time.Duration
+	bufferPath       string
+	buffer           *badger.DB
+}
+
+func (b ZMSDefaultBuffer) InitBuffer(bufferPath string, ttl int64) {
+	b.offlineBufferTTL = time.Duration(ttl) * time.Hour
+	if b.offlineBufferTTL > 0 {
+		db, err := badger.Open(badger.DefaultOptions(
+			path.Join(b.bufferPath),
+		).WithLogger(logger.Default()))
+		logger.Debug("Initialized BadgerDB for offline buffering", slog.String("path", path.Join(b.bufferPath)))
+		if err != nil {
+			logger.Error("Failed to open BadgerDB for offline buffering", slog.Any("error", err))
+		}
+		b.buffer = db
+	}
+}
+
+// Cleanup releases resources held by the baseObserver.
+// If offlineBufferTTL is greater than zero, it closes the buffer to free associated resources.
+func (b ZMSDefaultBuffer) Cleanup() {
+	if b.buffer != nil {
+		b.buffer.Close()
+	}
+}
+
+func (b ZMSDefaultBuffer) BufferHistory(history []zbx.History) (err error) {
+	return saveToBuffer[zbx.History](b.buffer, history, b.offlineBufferTTL)
+}
+func (b ZMSDefaultBuffer) FetchHistory(number int) (history []zbx.History, err error) {
+	return fetchfromBuffer[zbx.History](b.buffer, number)
+}
+func (b ZMSDefaultBuffer) DeleteHistory(history []zbx.History) (err error) {
+	return deleteFromBuffer[zbx.History](b.buffer, history)
+}
+func (b ZMSDefaultBuffer) BufferTrends(trends []zbx.Trend) (err error) {
+	return saveToBuffer(b.buffer, trends, b.offlineBufferTTL)
+}
+func (b ZMSDefaultBuffer) FetchTrends(number int) (trends []zbx.Trend, err error) {
+	return fetchfromBuffer[zbx.Trend](b.buffer, number)
+}
+func (b ZMSDefaultBuffer) DeleteTrends(trends []zbx.Trend) (err error) {
+	return deleteFromBuffer[zbx.Trend](b.buffer, trends)
+}
+func (b ZMSDefaultBuffer) BufferEvents(events []zbx.Event) (err error) {
+	return saveToBuffer(b.buffer, events, b.offlineBufferTTL)
+}
+func (b ZMSDefaultBuffer) FetchEvents(number int) (events []zbx.Event, err error) {
+	return fetchfromBuffer[zbx.Event](b.buffer, number)
+}
+func (b ZMSDefaultBuffer) DeleteEvents(events []zbx.Event) (err error) {
+	return deleteFromBuffer[zbx.Event](b.buffer, events)
+}
+
+func saveToBuffer[T zbx.Export](buffer *badger.DB, toBuffer []T, offlineBufferTTL time.Duration) (err error) {
+	var value bytes.Buffer
+	enc := gob.NewEncoder(&value)
+	txn := buffer.NewTransaction(true)
+	for _, item := range toBuffer {
+		key := item.Hash()
+		enc.Encode(item)
+		e := badger.NewEntry(key, value.Bytes()).WithTTL(offlineBufferTTL)
+		if err := txn.SetEntry(e); err == badger.ErrTxnTooBig {
+			err = txn.Commit()
+			if err != nil {
+				logger.Error("commit error")
+			}
+			txn := buffer.NewTransaction(true)
+			txn.SetEntry(e)
+		}
+	}
+	err = txn.Commit()
+	return
+}
+
+func fetchfromBuffer[T zbx.Export](buffer *badger.DB, batchSize int) (buffered []T, err error) {
+	opts := badger.DefaultIteratorOptions
+	opts.PrefetchSize = batchSize
+	txn := buffer.NewTransaction(false)
+	defer txn.Discard()
+	it := txn.NewIterator(opts)
+	defer it.Close()
+
+	decodeFunc := func(val []byte) (T, error) {
+		var t T
+		dec := gob.NewDecoder(bytes.NewReader(val))
+		err := dec.Decode(&t)
+		return t, err
+	}
+
+	for it.Rewind(); it.Valid(); it.Next() {
+		item := it.Item()
+		val, err := item.ValueCopy(nil)
+		if err != nil {
+			logger.Error("Failed to copy value from buffer", slog.String("observer", buffer.Opts().Dir), slog.Any("error", err))
+			continue
+		}
+		decoded, err := decodeFunc(val)
+		if err != nil {
+			logger.Error("Failed to decode from buffer", slog.String("observer", buffer.Opts().Dir), slog.Any("error", err))
+			continue
+		}
+		var zero T
+		if decoded.GetExportName() == zero.GetExportName() {
+			buffered = append(buffered, decoded)
+			if len(buffered) >= batchSize {
+				break
+			}
+		}
+	}
+
+	return
+}
+
+func deleteFromBuffer[T zbx.Export](buffer *badger.DB, buffered []T) (err error) {
+	txn := buffer.NewTransaction(true)
+	defer txn.Discard()
+	for _, item := range buffered {
+		key := item.Hash()
+		err = txn.Delete(key)
+		if err != nil {
+			logger.Error("Failed to delete from buffer", slog.String("observer", buffer.Opts().Dir), slog.Any("error", err))
+		}
+	}
+	err = txn.Commit()
+	if err != nil {
+		logger.Error("Failed to commit", slog.String("observer", buffer.Opts().Dir), slog.Any("error", err))
+	}
+
+	return
+}
